899a21a937df522a4e1de87dac3bac5de8adedb4
Rishabh Gupta <belikerishabh@gmail.com>
2020-03-22 12:41:03 +0530

    SFss

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 5000;
 const app = express();
 
 app.use(cors());
+app.use(bodyParser.json());
 
 mongoose
   .connect('mongodb+srv://imrishabh18:1234@hotornot-xmw8q.mongodb.net/test?retryWrites=true&w=majorityt', { useNewUrlParser: true })
@@ -14,3 +16,29 @@ mongoose
 app.listen(PORT ,() => {
     console.log("server running");
 })
+
+const schema = new mongoose.Schema({
+  gender: Number,
+  arr: [
+    {
+      id: String,
+      gender: Number,
+      hot: Number,
+      not: Number
+    }
+  ]
+});
+
+const horn = mongoose.model("horn",schema);
+
+app.post("/horn", (req, res) => {
+  const newHorn = new horn({
+    arr: req.body
+  });
+  console.log(newHorn);
+  newHorn.save().then(likes => res.json(likes));
+});
+
+app.get("/horn", (req, res) => {
+  horn.find().then(likes => res.json(likes));
+});
